74c0664360edd1f5d4bd377c8e7efa038e218215
DaffaDwiyanti <daffa.dwiyanti@gmail.com>
2019-05-13 11:31:27 +0700

    bisaa

--- app/Models/Destinasi.php
+++ app/Models/Destinasi.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 class Destinasi extends Model
 {
     protected $table="tb_destinasi";
-    protected $fillable = ['id_destinasi','nama_destinasi','lokasi_destinasi','harga_destinasi','persyaratan','deskripsi','ketinggian'];
+    protected $fillable = ['id','nama_destinasi','lokasi_destinasi','harga_destinasi','persyaratan','deskripsi','ketinggian'];
 }
